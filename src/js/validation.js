afe911210d6c7f47ae743a99bd04c790cf6fc474
Semykin Dmitry <dgsemykin@gmail.com>
2019-11-29 18:46:04 +0300

    start create error and success statusesfor payment form

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,16 +1,16 @@
 
 var card = document.querySelector('.card-form__number input');
-// card.addEventListener('keydown', {
-//   alert('hi');
-// });
+var icon = document.querySelector('.field__type-icon');
+
 card.onkeydown = function (){
-  console.log(card.value[0]);
-if (card.value[0] = '4') {
-  console.log('VISA');
-} else if (card.value[0] = '5') {
-  console.log('MASTERCARD');
-} else if (card.value[0] = '6') {
-  console.log('MAESTRO');
-}
+  if (card.value === '4444   4444   ____   ____') {
+    icon.classList.add('_visa');
+  } else if (card.value === '5555   5555   ____   ____') {
+    icon.classList.add('_mastercard');
+  } else if (card.value === '6666   6666   ____   ____') {
+    icon.classList.add('_maestro');
+  } else if (card.value === '') {
+    icon.classList.remove('_visa', '_mastercard', '_maestro');
+  }
 };
 
