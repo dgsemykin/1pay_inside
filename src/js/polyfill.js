4b4a6b95541097c41a732d73c02dbff9f5c2bb91
Dmitry Semykin <dgsemykin@gmail.com>
2019-12-17 00:01:51 +0300

    change includes

--- src/js/polyfill.js
+++ src/js/polyfill.js
@@ -32,17 +32,45 @@ if (!Array.from) {
 }
 
 //Array.includes()
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
+(function(undefined) {
 
-    if (search instanceof RegExp) {
-      throw TypeError('first argument must not be a RegExp');
-    }
-    if (start === undefined) { start = 0; }
-    return this.indexOf(search, start) !== -1;
-  };
-}
+// Array.prototype.includes
+  (function () {
+    Object.defineProperty(Array.prototype, 'includes', {
+      configurable: true,
+      value: function includes (searchElement /*, fromIndex*/) {
+        'use strict';
+        var O = Object(this);
+        var len = parseInt(O.length) || 0;
+        if (len === 0) {
+          return false;
+        }
+        var n = parseInt(arguments[1]) || 0;
+        var k;
+        if (n >= 0) {
+          k = n;
+        } else {
+          k = len + n;
+          if (k < 0) {
+            k = 0;
+          }
+        }
+        var currentElement;
+        while (k < len) {
+          currentElement = O[k];
+          if (searchElement === currentElement ||
+              (searchElement !== searchElement && currentElement !== currentElement)) {
+            return true;
+          }
+          k++;
+        }
+        return false;
+      },
+      writable: true
+    });
+  }());
+})
+    .call('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
 
 // Object.assign
 if (!Object.assign) {
