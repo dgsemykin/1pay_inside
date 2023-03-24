2cbbe6cce7ed5f7129f075db6fce3e2ef6b6d5f7
Semykin Dmitry <dgsemykin@gmail.com>
2023-03-24 23:47:41 +0300

    finish header 2

--- src/js/currencies-checker-v2.js
+++ src/js/currencies-checker-v2.js
@@ -1,32 +1,46 @@
 const amountBlockMain = document.querySelector('.profile-header__amount');
 const activeBlock = document.querySelector('.profile-header__active-block-v2');
-const fullBlock = document.querySelector('.profile-header__full-block-v2');
+let fullBlock = document.querySelector('.profile-header__full-block-v2');
 
 const amountBlocks = document.querySelectorAll('.profile-header__amount-block-v2');
 
-amountBlockMain.addEventListener('click', function(event) {
+let amoutFromFullBlock = fullBlock.querySelectorAll('.profile-header__amount-block-v2');
+
+amountBlockMain.addEventListener('click', function() {
   amountBlockMain.classList.toggle('profile-header__amount_active');
 })
 
-amountBlocks.forEach(function(amountBlock) {
-  amountBlock.addEventListener('click', function(event) {
-    console.log(activeBlock);
-    let activeContent = activeBlock.querySelector('.profile-header__amount-block-v2');
-    console.log('active content is' + activeContent);
-    console.log(event.target.parentElement)
-
-    let html;
-    if(event.target.parentElement.className === 'profile-header__full-block-v2') {
-      html = event.target;
-    } else if (event.target.parentElement.id === '') {
-      html = event.target.parentElement.parentElement.parentElement;
-    } else {
-      html = event.target.parentElement;
-    }
-    // const htmlClone = html.cloneNode(true);
-    activeBlock.innerHTML = '';
-    activeBlock.appendChild(html);
-    fullBlock.appendChild(activeContent);
-  })
+amoutFromFullBlock.forEach(function(amountBlock) {
+  amountBlock.addEventListener('click', checker)
 })
+
+function checker(event) {
+  let activeContent = activeBlock.querySelector('.profile-header__amount-block-v2');
+
+  let html ;
+  if(event.target.parentElement.className === 'profile-header__full-block-v2') {
+    html = event.target;
+  }  else if (event.target.parentElement.className === 'profile-header__active-block-v2') {
+    html = event.target.parentElement;
+  } else if (event.target.parentElement.id === '') {
+    html = event.target.parentElement.parentElement.parentElement;
+  } else {
+    html = event.target.parentElement;
+  }
+  activeBlock.innerHTML = '';
+  activeBlock.appendChild(html);
+  fullBlock.appendChild(activeContent);
+
+  setTimeout(function(){
+    activeContent = activeBlock.querySelector('.profile-header__amount-block-v2');
+    activeContent.removeEventListener('click', checker);
+  },100);
+
+  fullBlock = document.querySelector('.profile-header__full-block-v2');
+  amoutFromFullBlock = fullBlock.querySelectorAll('.profile-header__amount-block-v2');
+
+  amoutFromFullBlock.forEach(function(amountBlock) {
+    amountBlock.addEventListener('click', checker)
+  })
+}
 
